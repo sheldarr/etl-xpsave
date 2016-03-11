91985bf2e5d669aad4428b16ce0c3e2e94054734
sheldar@wp.pl <sheldar@wp.pl>
2016-03-11 16:24:19 +0100

    Unified quotes

--- xpsave.lua
+++ xpsave.lua
@@ -92,47 +92,47 @@ function et.G_Printf(...)
 end
 
 function sendMessageToPlayer(player, message, ...)
-    et.trap_SendServerCommand(player.number, 'cpm\"' .. string.format(message, ...) .. '\n\"')
+    et.trap_SendServerCommand(player.number, "cpm\"" .. string.format(message, ...) .. "\n\"")
 end
 
 function et_InitGame(levelTime, randomSeed, restart)
-    et.G_Printf('et_InitGame [%d] [%d] [%d]\n', levelTime, randomSeed, restart)
+    et.G_Printf("et_InitGame [%d] [%d] [%d]\n", levelTime, randomSeed, restart)
     et.RegisterModname(MOD_NAME)
 end
 
 function et_ShutdownGame(restart)
-    et.G_Printf('et_ShutdownGame [%s]\n', restart)
+    et.G_Printf("et_ShutdownGame [%s]\n", restart)
 
     saveXpForAllPlayers()
 end
 
 function et_RunFrame(levelTime)
     if levelTime % 1000 == 0 then
-        et.G_Printf('et_RunFrame [%d]\n', levelTime)
+        et.G_Printf("et_RunFrame [%d]\n", levelTime)
         saveXpForAllPlayers()
     end
 end
 
 -- return 1 if intercepted, 0 if passthrough
 function et_ClientCommand(clientNumber, command)
-    et.G_Printf('et_ClientCommand: [%d] [%d] [%s]\n', clientNumber, et.trap_Argc(), command)
+    et.G_Printf("et_ClientCommand: [%d] [%d] [%s]\n", clientNumber, et.trap_Argc(), command)
     return 0
 end
 
 -- return 1 if intercepted, 0 if passthrough
 function et_ConsoleCommand()
-    et.G_Printf('et_ConsoleCommand: [%s] [%s]\n', et.trap_Argc(), et.trap_Argv(0))
+    et.G_Printf("et_ConsoleCommand: [%s] [%s]\n", et.trap_Argc(), et.trap_Argv(0))
     return 0
  end
 
 function et_ClientConnect(clientNumber, firstTime, isBot)
-    et.G_Printf( 'et_ClientConnect: [%d] [%d] [%d]\n', clientNumber, firstTime, isBot)
+    et.G_Printf( "et_ClientConnect: [%d] [%d] [%d]\n", clientNumber, firstTime, isBot)
 
     return nil
 end
 
 function et_ClientDisconnect(clientNumber)
-    et.G_Printf( 'et_ClientDisconnect: [%d]\n', clientNumber)
+    et.G_Printf( "et_ClientDisconnect: [%d]\n", clientNumber)
 
     local player = getPlayer(clientNumber)
 
@@ -142,12 +142,12 @@ end
 function et_ClientBegin(clientNumber)
     local player = getPlayer(clientNumber)
 
-    et.G_Printf( 'et_ClientBegin: [%d] %s\n', clientNumber, player.name)
-    sendMessageToPlayer(player, 'Welcome %s \n', player.name)
+    et.G_Printf( "et_ClientBegin: [%d] %s\n", clientNumber, player.name)
+    sendMessageToPlayer(player, "Welcome %s \n", player.name)
 
     loadXpForPlayer(player)
 end
 
 function et_ClientUserinfoChanged(clientNumber)
-       et.G_Printf( 'et_ClientUserinfoChanged: [%d] = [%s]\n', clientNumber, et.trap_GetUserinfo(clientNumber))
+       et.G_Printf( "et_ClientUserinfoChanged: [%d] = [%s]\n", clientNumber, et.trap_GetUserinfo(clientNumber))
 end
